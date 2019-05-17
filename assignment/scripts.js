4a8b9680b8e991dcc11355ed41fb6405e8cd0d69
KaetiG <kaetigust@gmail.com>
2019-05-17 16:22:16 -0500

    added notes for myself

--- assignment/scripts.js
+++ assignment/scripts.js
@@ -6,11 +6,13 @@ $('.submitButton').on('click', submitButtonHandler);
 }
 
 function submitButtonHandler(){
+//declares local variable to use later
     let empFirst= $('#firstNameInput').val();
     let empLast= $('#lastNameInput').val();
     let empId= $('#idInput').val();
     let empTitle= $('#titleInput').val();
     let empSalary= $('#salaryInput').val();
+//adds new row using inputs from text fields plus button
     $('#empTableBody').append(`<tr>
         <td>${empFirst}</td>
         <td>${empLast}</td>
@@ -18,6 +20,14 @@ function submitButtonHandler(){
         <td>${empTitle}</td>
         <td>${empSalary}</td>
         <td><button class="deleteButton">Delete</button></tr>`);
+
+//clears all text box fields for new entries
+    $('#firstNameInput').val(' ');
+    $('#lastNameInput').val(' ');
+    $('#idInput').val(' ');
+    $('#titleInput').val(' '); 
+    $('#salaryInput').val(' ');
+//REMINDER: google how to get placeholder text back lol   
 }
 
 //function deleteButtonHandler(){
