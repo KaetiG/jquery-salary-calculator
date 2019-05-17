de7ff67d7ad16ac294d26ef89495a8a79ce955f5
KaetiG <kaetigust@gmail.com>
2019-05-17 16:13:33 -0500

    on submit button click-new employee info adds to table

--- assignment/scripts.js
+++ assignment/scripts.js
@@ -1,4 +1,25 @@
 $(document).ready(readyNow);
+
 function readyNow(){
     console.log( 'Page Ready');
+$('.submitButton').on('click', submitButtonHandler);
 }
+
+function submitButtonHandler(){
+    let empFirst= $('#firstNameInput').val();
+    let empLast= $('#lastNameInput').val();
+    let empId= $('#idInput').val();
+    let empTitle= $('#titleInput').val();
+    let empSalary= $('#salaryInput').val();
+    $('#empTableBody').append(`<tr>
+        <td>${empFirst}</td>
+        <td>${empLast}</td>
+        <td>${empId}</td>
+        <td>${empTitle}</td>
+        <td>${empSalary}</td>
+        <td><button class="deleteButton">Delete</button></tr>`);
+}
+
+//function deleteButtonHandler(){
+
+//}
